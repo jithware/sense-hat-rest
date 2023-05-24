f57964740e8543429010b3cabd9d73da73f926c5
jithware <jithware@gmail.com>
2023-05-24 09:26:29 -0400

    Update testing

--- test.sh
+++ test.sh
@@ -4,7 +4,8 @@
 HOST="localhost"
 PORT="8080"
 URL="http://$HOST:$PORT"
-CURL="curl --write-out '%{http_code}' --silent --output /dev/null"
+CURLRESP="curl -I --write-out '%{http_code}' --silent --output /dev/null"
+CURL="curl -sS"
 
 RRDSENSORS=('humidity' 'temperature' 'pressure' 'compass' 'temperature_cpu')
 LIVESENSORS=(${RRDSENSORS[@]} 'temperature_from_humidity' 'temperature_from_pressure' 'orientation_radians' 'orientation_degrees'  'orientation' 'compass_raw' 'gyroscope' 'gyroscope_raw' 'accelerometer' 'accelerometer_raw')
@@ -14,44 +15,66 @@ HTML=(${IMAGES[@]} 'all')
 DISPLAY=(${IMAGES[@]})
 
 get() {
-    response=$(eval $CURL $1)
-    if [[ "$response" -ne 200 ]] ; then
+    response=$(eval $CURLRESP $1)
+    if [ "$response" -ne 200 ] ; then
         echo "Failed ($response): $1"
         exit 1
     fi
     echo "Success (200): $1"
 }
 
+getjson() {
+     eval $CURL $1 | python -mjson.tool &> /dev/null
+     if [ $? -ne 0 ]; then
+        echo "Failed (json): $1"
+        exit 1
+     fi
+     echo "Success (json): $1"
+}
+
+getimage() {
+     if command -v identify &> /dev/null; then
+          eval $CURL $1 | identify - &> /dev/null
+          if [ $? -ne 0 ]; then
+               echo "Failed (image): $1"
+               exit 1
+          fi
+          echo "Success (image): $1"
+     else
+          get "$1"
+     fi
+}
+
 # Index
-echo "Getting index..."
+echo "Testing index..."
 get "$URL"
 
 # Live sensors
-echo "Getting live sensors..."
+echo "Testing live sensors..."
 for sensor in "${LIVESENSORS[@]}"; do
-     get "$URL/live/$sensor" 
+     getjson "$URL/live/$sensor"
 done
 
 # Past sensors
-echo "Getting past sensors..."
+echo "Testing past sensors..."
 for sensor in "${PASTSENSORS[@]}"; do
-     get "$URL/past/$sensor" 
+     getjson "$URL/past/$sensor"
 done
 
 # Images
-echo "Getting images..."
+echo "Testing images..."
 for sensor in "${IMAGES[@]}"; do
-     get "$URL/image/$sensor" 
+     getimage "$URL/image/$sensor"
 done
 
 # Html
-echo "Getting htmls..."
+echo "Testing htmls..."
 for sensor in "${HTML[@]}"; do
      get "$URL/html/$sensor" 
 done
 
 # Display
-echo "Getting displays..."
+echo "Testing displays..."
 for sensor in "${DISPLAY[@]}"; do
      get "$URL/display/$sensor" 
 done
