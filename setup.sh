b323703fd208e60b72491f2716ca85a93f3d1eaf
jithware <jithware@gmail.com>
2023-05-25 09:36:51 -0400

    Update install directories

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ sudo apt-get -y install python3-sense-hat rrdtool python3-rrdtool python3-webpy 
 
 # copy over the files
 [ ! -f "/etc/sense-hat-rest.conf" ] && sudo install -v ./sense-hat-rest.conf /etc || echo "keeping existing '/etc/sense-hat-rest.conf' you may need to add new configurations"
-sudo install -v ./sense-hat-rest.py /usr/bin/ # rest api script
-sudo install -v ./sense-hat-rest.service /lib/systemd/system/ # rest api service
+sudo install -v -d /usr/share/sense-hat-rest/static/ # create directories
+sudo install -v -m 755 ./sense-hat-rest.py /usr/share/sense-hat-rest/ # rest api python script
+sudo install -v ./static/* /usr/share/sense-hat-rest/static/ # static files
+sudo install -v ./sense-hat-rest.service /lib/systemd/system/ # systemd service
 sudo install -v -d /var/cache/sense-hat # database path
 
 # enable service
